1cd43f2912441ecbe381a8727e1f5b3fa76f94cb
Aysegul Aydogan <a.aydogan2018@gtu.edu.tr>
2021-05-16 14:35:29 +0300

    Print function turns into 2 dimensional array

--- HW09/hw9.c
+++ HW09/hw9.c
@@ -24,23 +24,43 @@ typedef struct
 
 void Add_Customer(Customer *customer,int size);
 void print_infos(Customer *customer);
-void print_current_seq(Customer *customer, int customer_num);
+void print_current_seq(Customer customer[MAX_CUSTOMER][MAX_CUSTOMER], int *size, int seq_num);
 void process_customer(Customer *customer, int size);
+void order_seq(Customer *customer, int size, int last_label);
+
+
 
 int main(void)
 {
-    int cur_size = 5;
-    Customer customers[MAX_CUSTOMER] = {{"Jack",26,1},
+    int size[MAX_CUSTOMER] = {0};
+    size[0] = 5;
+    size[1] = 4;
+    int seq_num = 1;
+    // size[2] =
+    // {{1,2,3,4,5},{1,2,3,4}}
+    //size[2] = {5};
+    //seq_num = 0 1
+    //seq_num = 0
+    //size[seq_num] = 5
+
+    Customer customers[MAX_CUSTOMER][MAX_CUSTOMER] = {
+                                       {{"Jack",26,1},
                                         {"Henry",36,2},
                                         {"Muhammed",22,3},
                                         {"Aysegul",21,4},
-                                        {"Huseyin",22,0}};
+                                        {"Huseyin",22,0}}
+                                        ,
+                                        {{"Jack",26,0},
+                                        {"Henry",36,3},
+                                        {"Muhammed",22,2},
+                                        {"Aysegul",21,4}
+                                        }};
     while (1)
     {
         int choice;
         printf("**********Banking System**********\n");
         printf("Current Sequence:");
-        print_current_seq(customers,cur_size);
+        print_current_seq(customers,size,seq_num);
         printf("\n");
         printf("1-Add customer\n");
         printf("2-Process customer\n");
@@ -48,20 +68,20 @@ int main(void)
         scanf("%d",&choice);
         if (choice == 1)
         {
-            Add_Customer(customers,cur_size);
-            cur_size++;
+            // Add_Customer(customers,cur_size);
+            // cur_size++;
         }
         
         else if (choice == 2)
         {
-            if(cur_size == 0)
+            if(size == 0)
             {
                 printf("No customer\n");
             }
             else
             {
-                process_customer(customers,cur_size);
-                cur_size--;
+                // process_customer(customers,cur_size);
+                // cur_size--;
             }
         }
         else if (choice == 0)
@@ -71,6 +91,7 @@ int main(void)
         }
         else
             printf("INVALID\n");
+        
     }
 }
 
@@ -83,6 +104,7 @@ void Add_Customer(Customer *customer, int size)
     scanf(" %d",&customer[size].age);
     printf("\nPlease enter the label of the customer:");
     scanf(" %u",&customer[size].label);
+    order_seq(customer,size,customer[size].label);
 }
 
 void print_infos(Customer *customer)
@@ -92,14 +114,17 @@ void print_infos(Customer *customer)
     printf("The label: %u\n",customer->label);
 }
 
-void print_current_seq(Customer *customer, int size)
+
+void print_current_seq(Customer customer[MAX_CUSTOMER][MAX_CUSTOMER], int *size, int seq_num)
 {
-    for (int i = 0; i < size; i++)
+    for (int i = 0; i <= seq_num; i++)
     {
-        printf("%d-",customer[i].label);
+        for (int j = 0; j < size[i]; j++)
+        {
+            printf("%d-",customer[i][j].label);
+        }
     }
     printf("\n");
-    
 }
 
 void process_customer(Customer *customer, int size)
@@ -109,4 +134,30 @@ void process_customer(Customer *customer, int size)
     {
         customer[i] = customer[i+1];
     }
+}
+void order_seq(Customer *customer, int size, int last_label)
+{
+    Customer temp;
+    int num_label = 0;
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = i+1; j < size; j++)
+        {
+            if (customer[i].label > customer[j].label)
+            {
+                temp = customer[i];
+                customer[i] = customer[j];
+                customer[j] = temp;
+            } 
+        }
+        if(last_label == customer[i].label)
+        {
+            num_label++;
+        }
+    }
+    if(last_label == 0 && num_label == 5)
+    {
+        
+    }
+
 }
